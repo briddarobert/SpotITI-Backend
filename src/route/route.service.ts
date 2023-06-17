08454dd4a77bad0e7a00613054cc520b3ab33ea3
Robert Bridda <briddarobert@icloud.com>
2023-06-17 15:27:29 +0200

    Closes #1

--- src/route/route.service.ts
+++ src/route/route.service.ts
@@ -82,7 +82,7 @@ export class RouteService {
         excludedNodes,
         excludedLinkTypes,
       );
-      if (!route || route.lenght < currentRoute.lenght) route = currentRoute;
+      if (!route || currentRoute.lenght < route.lenght) route = currentRoute;
     }
     if (!route) {
       throw RouteService.noPathFoundError;
